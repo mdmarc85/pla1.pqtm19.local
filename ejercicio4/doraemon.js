9d7e4fd326e014660adc2e38236cc8f61197e0a8
tarda <tarda@DESKTOP-VMAFLTC>
2019-09-19 20:27:02 +0200

    go22

---
+++ ejercicio4/doraemon.js
@@ -0,0 +1,20 @@
+///**
+//*
+//*/
+//
+//var doraemon3;
+//
+//doraemon3 = document.getElementById("doraemon3");
+//
+//doraemon3.onclick = function (e){
+//	alert("hola");
+//	doraemon3.style.left="400px";
+//};
+
+var doraemons;
+
+doraemons = document.querySelectorAll("#fons>div");
+
+console.log(doraemons);
+
+for(i=0 ; )
